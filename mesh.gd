b18f5a1051265d86e520ab3a9fd066944d3e37c4
Varun Somashekar <113564517+vaaar@users.noreply.github.com>
2024-09-14 15:58:06 -0400

    add helper function to add tp meshes to scene

--- mesh.gd
+++ mesh.gd
@@ -6,72 +6,54 @@ extends MeshInstance3D
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	#pass # Replace with function body.
+	#$Camera
 	create_mesh()
 	pass
 
 func create_mesh() -> void:
 	var array_mesh = ArrayMesh.new()
-	#var vertices := PackedVector3Array(
+	
+	#var test_vertices = PackedVector3Array(
 		#[
-			#Vector3(-1,0,-1),
-			#Vector3(1,0,-1),
-			#Vector3(1,0,1),
-			#
-			#Vector3(-1,0,1),
-			#
-			#Vector3(0,0,1),
-			#Vector3(1,0,0)
+			#Vector3(-10,0, 6),
+			#Vector3(-5,0, 10),
+			#Vector3(1,0, 11),
+			#Vector3(5,0, 5),
+			#Vector3(5,0, -5),
+			#Vector3(-6,0, 1),
 		#]
 	#)
-	#var indices := PackedInt32Array(
-		#[
-			#0,1,2,
-			#
-			##0,2,3
-			#
-			#
-		#]
-	#)
-	#
-	#var indices2 := PackedInt32Array(
-		#[0,2,3]
-	#)
-	#
-	#var array = []
-	#array.resize(Mesh.ARRAY_MAX)
-	#array[Mesh.ARRAY_VERTEX] = vertices
-	#array[Mesh.ARRAY_INDEX] = indices
-	#array_mesh.add_surface_from_arrays(Mesh.PRIMITIVE_TRIANGLES, array)
-	#
-	#array[Mesh.ARRAY_INDEX] = indices2
-	#array_mesh.add_surface_from_arrays(Mesh.PRIMITIVE_TRIANGLES, array)
 	
 	var test_vertices = PackedVector3Array(
 		[
-			Vector3(-10,0, 6),
-			Vector3(-5,0, 10),
-			Vector3(1,0, 11),
-			Vector3(5,0, 5),
-			Vector3(5,0, -5),
-			Vector3(-6,0, 1),
+			Vector3(-5, 0, 5),
+			Vector3(5, 0, 5),
+			Vector3(-5, 0, -5)
 		]
 	)
 	
-	#var test_vertices = PackedVector3Array([
-		#Vector3(-5,0,5),
-		#Vector3(5,0,5),
-		#Vector3(5,0,-5),
-		##Vector3(-5,0,-5),
-	#])
-	
 	var tp = TriangularPlane.new(test_vertices)
 	
-	var surf = tp.export_surface()
+	#var surf = tp.export_surface()
+	#
+	#var array = surf["mesh_array"]
+	#array_mesh.add_surface_from_arrays(surf["primitive"], array)
+	#
+	#mesh = array_mesh
 	
-	var array = surf["mesh_array"]
-	array_mesh.add_surface_from_arrays(surf["primitive"], array)
+	recreate_plane_scene([tp])
 	
-	mesh = array_mesh
+	self.global_transform.origin = Vector3(0,0,0)
+	
+func recreate_plane_scene(planes):
+	var new_arraymesh = ArrayMesh.new()
+	
+	for p: TriangularPlane in planes:
+		var exp = p.export_surface()
+		var next_mesh = exp["mesh_array"]
+		new_arraymesh.add_surface_from_arrays(Mesh.PRIMITIVE_TRIANGLES, next_mesh)
+	
+	mesh = new_arraymesh
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
