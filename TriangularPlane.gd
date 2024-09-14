467356fc0b4e1e806a261869bec577370629b3ba
Varun Somashekar <113564517+vaaar@users.noreply.github.com>
2024-09-14 14:40:18 -0400

    ok actually fix merge conflci

--- TriangularPlane.gd
+++ TriangularPlane.gd
@@ -1,32 +1,13 @@
 class_name TriangularPlane
 
-<<<<<<< HEAD
-var points_arr = PackedVector3Array()
-var indices_arr = PackedInt32Array()
-=======
 var _points_arr = PackedVector3Array()
 var _indices_arr = PackedInt32Array()
->>>>>>> 0c830aa (change var names in triangularplane)
 
 func _init(plane_points: PackedVector3Array):
 	for index in len(plane_points):
 		var curr_point = plane_points[index]
 		
 		if index > 1:
-<<<<<<< HEAD
-			indices_arr.push_back(0)
-			indices_arr.push_back(index)
-			indices_arr.push_back(index - 1)
-			
-		points_arr.push_back(curr_point)
-	
-	print("Hello world\n")	
-	print("Points:")
-	print(points_arr)
-	
-	print("Indices:")
-	print(indices_arr)
-=======
 			_indices_arr.push_back(0)
 			_indices_arr.push_back(index)
 			_indices_arr.push_back(index - 1)
@@ -39,27 +20,16 @@ func _init(plane_points: PackedVector3Array):
 	
 	print("Indices:")
 	print(_indices_arr)
->>>>>>> 0c830aa (change var names in triangularplane)
 	
 func export_surface():
 	var array = []
 	array.resize(Mesh.ARRAY_MAX)
-<<<<<<< HEAD
-	array[Mesh.ARRAY_VERTEX] = points_arr
-	array[Mesh.ARRAY_INDEX] = indices_arr
-=======
 	array[Mesh.ARRAY_VERTEX] = _points_arr
 	array[Mesh.ARRAY_INDEX] = _indices_arr
->>>>>>> 0c830aa (change var names in triangularplane)
 	
 	return {
 		"primitive": Mesh.PRIMITIVE_TRIANGLES,
 		"mesh_array": array,
-<<<<<<< HEAD
-		"points": points_arr,
-		"indices": indices_arr,
-=======
 		"points": _points_arr,
 		"indices": _indices_arr,
->>>>>>> 0c830aa (change var names in triangularplane)
 	}
--- mesh.gd
+++ mesh.gd
@@ -45,35 +45,6 @@ func create_mesh() -> void:
 	#
 	#array[Mesh.ARRAY_INDEX] = indices2
 	#array_mesh.add_surface_from_arrays(Mesh.PRIMITIVE_TRIANGLES, array)
-<<<<<<< HEAD
-	
-	#var test_vertices = PackedVector3Array(
-		#[
-			#Vector3(-10,0, 6),
-			#Vector3(-5,0, 10),
-			#Vector3(1,0, 11),
-			#Vector3(5,0, 5),
-			#Vector3(5,0, -5),
-			#Vector3(-6,0, 1),
-		#]
-	#)
-	
-	var test_vertices = PackedVector3Array([
-		Vector3(-5,0,5),
-		Vector3(5,0,5),
-		Vector3(5,0,-5),
-		#Vector3(-5,0,-5),
-	])
-	
-	var tp = TriangularPlane.new(test_vertices)
-	var ind_arr = tp.indices_arr
-	var vert_arr = tp.points_arr
-	var array = []
-	array.resize(Mesh.ARRAY_MAX)
-	array[Mesh.ARRAY_VERTEX] = vert_arr
-	array[Mesh.ARRAY_INDEX] = ind_arr
-	array_mesh.add_surface_from_arrays(Mesh.PRIMITIVE_TRIANGLES, array)
-=======
 	
 	var test_vertices = PackedVector3Array(
 		[
@@ -99,7 +70,6 @@ func create_mesh() -> void:
 	
 	var array = surf["mesh_array"]
 	array_mesh.add_surface_from_arrays(surf["primitive"], array)
->>>>>>> 0c830aa (change var names in triangularplane)
 	
 	mesh = array_mesh
 
