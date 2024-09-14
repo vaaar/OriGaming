3e8bec3c23dbed9f5638f74dac5b271380c0f53d
Zenza235 <paoloatienza235@gmail.com>
2024-09-14 16:20:27 -0400

    test fold function; it works!

--- TransformFold.gd
+++ TransformFold.gd
@@ -1,24 +1,30 @@
 extends Node
-
+class_name TransformFold
 
 func fold(
 	fl_start: Vector3, fl_end: Vector3, 
 	points_to_transform_i, ## Array[[plane index, point index]]
-	planes: Array[TriangularPlane]) -> Array[TriangularPlane]:
+	planes: Array[TriangularPlane]
+) -> Array[TriangularPlane]:
+	print("hello")
+	
 	var res: Array[TriangularPlane] = []
 	var vertices_arr: Array[PackedVector3Array] = []
 	
 	for plane in planes:
-		vertices_arr.append(plane.export_surface()["vertices"])
+		vertices_arr.append(plane.export_surface()["points"])
 	
 	var angle = PI
 	var axis: Vector3 = (fl_end - fl_start).normalized()
 	
-	## index[0] = plane index
-	## index[1] = point index
 	for index in points_to_transform_i:
-		var new_point = res[index[0]][index[1]].rotated(axis, angle)
-		res[index[0]].set(index[1], new_point)
+		#print("(plane " + index[0] + ", point " + index[1] + "): \n")
+		#print(res[index[0]][index[1]] + " -> ")
+		
+		var new_point = vertices_arr[index[0]][index[1]].rotated(axis, angle)
+		vertices_arr[index[0]].set(index[1], new_point)
+		
+		#print(res[index[0]][index[1]])
 		
 	for vertices in vertices_arr:
 		res.append(TriangularPlane.new(vertices))
--- mesh.gd
+++ mesh.gd
@@ -11,67 +11,65 @@ func _ready() -> void:
 
 func create_mesh() -> void:
 	var array_mesh = ArrayMesh.new()
-	#var vertices := PackedVector3Array(
+	
+	#var test_vertices = PackedVector3Array(
 		#[
-			#Vector3(-1,0,-1),
-			#Vector3(1,0,-1),
-			#Vector3(1,0,1),
-			#
-			#Vector3(-1,0,1),
-			#
-			#Vector3(0,0,1),
-			#Vector3(1,0,0)
+			#Vector3(-10,0, 6),
+			#Vector3(-5,0, 10),
+			#Vector3(1,0, 11),
+			#Vector3(5,0, 5),
+			#Vector3(5,0, -5),
+			#Vector3(-6,0, 1),
 		#]
 	#)
-	#var indices := PackedInt32Array(
-		#[
-			#0,1,2,
-			#
-			##0,2,3
-			#
-			#
-		#]
-	#)
+	
+	var p1_vertices = PackedVector3Array([
+		Vector3(-5,0,5),
+		Vector3(5,0,5),
+		Vector3(5, 0, 0),
+		Vector3(0, 0, -5),
+		Vector3(-5,0,-5),
+	])
+	
+	var p2_vertices = PackedVector3Array([
+		Vector3(5,0,0),
+		Vector3(5,0,-5),
+		Vector3(0,0,-5),
+	])
+	
+	var p1 = TriangularPlane.new(p1_vertices)
+	var p2 = TriangularPlane.new(p2_vertices)
+	
+	var surf1 = p1.export_surface()
+	var surf2 = p2.export_surface()
+	
+	array_mesh.add_surface_from_arrays(surf1["primitive"], surf1["mesh_array"])
+	
+	array_mesh.add_surface_from_arrays(Mesh.PRIMITIVE_TRIANGLES, surf2["mesh_array"])
 	#
-	#var indices2 := PackedInt32Array(
-		#[0,2,3]
-	#)
-	#
-	#var array = []
-	#array.resize(Mesh.ARRAY_MAX)
-	#array[Mesh.ARRAY_VERTEX] = vertices
-	#array[Mesh.ARRAY_INDEX] = indices
-	#array_mesh.add_surface_from_arrays(Mesh.PRIMITIVE_TRIANGLES, array)
-	#
-	#array[Mesh.ARRAY_INDEX] = indices2
-	#array_mesh.add_surface_from_arrays(Mesh.PRIMITIVE_TRIANGLES, array)
-	
-	var test_vertices = PackedVector3Array(
-		[
-			Vector3(-10,0, 6),
-			Vector3(-5,0, 10),
-			Vector3(1,0, 11),
-			Vector3(5,0, 5),
-			Vector3(5,0, -5),
-			Vector3(-6,0, 1),
-		]
-	)
-	
-	#var test_vertices = PackedVector3Array([
-		#Vector3(-5,0,5),
-		#Vector3(5,0,5),
-		#Vector3(5,0,-5),
-		##Vector3(-5,0,-5),
-	#])
-	
-	var tp = TriangularPlane.new(test_vertices)
-	
-	var surf = tp.export_surface()
-	
-	var array = surf["mesh_array"]
-	array_mesh.add_surface_from_arrays(surf["primitive"], array)
-	
 	mesh = array_mesh
+	
+	var new_planes = TransformFold.new().fold(Vector3(5, 0, 0), Vector3(0, 0, -5), [[1, 1]], [p1, p2])
+	
+	#for i in new_planes.length():
+		#var curr_plane = new_planes[i]
+		#
+		#print("Plane: ")
+		#print(i)
+		#print("Points: ")
+		#print(curr_plane.export_surface()["points"])
+			
+	var array_mesh1 = ArrayMesh.new()
+	var surf11 = new_planes[0].export_surface()
+	var surf21 = new_planes[1].export_surface()
+	
+	var mesh11 = surf11["mesh_array"]
+	array_mesh1.add_surface_from_arrays(surf11["primitive"], surf11["mesh_array"])
+	
+	array_mesh1.add_surface_from_arrays(surf21["primitive"], surf21["mesh_array"])
+	
+	mesh = array_mesh1
+	
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
