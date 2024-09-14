c755732cd6582f690b038810866a9a3346371a7e
Varun Somashekar <113564517+vaaar@users.noreply.github.com>
2024-09-14 10:33:24 -0400

    Update

--- mesh.gd
+++ mesh.gd
@@ -1,16 +1,44 @@
+@tool
 extends MeshInstance3D
 
+@export var update = false 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	var surface_array = []
-	surface_array.resize(Mesh.ARRAY_MAX)
+	#pass # Replace with function body.
+	create_mesh()
+	pass
+
+func create_mesh() -> void:
+	var array_mesh = ArrayMesh.new()
+	var vertices := PackedVector3Array(
+		[
+			Vector3(-1,0,-1),
+			Vector3(1,0,-1),
+			Vector3(1,0,1),
+			
+			Vector3(-1,0,1),
+			
+			Vector3(0,0,1),
+			Vector3(1,0,0)
+		]
+	)
+	var indices := PackedInt32Array(
+		[
+			0,1,2,
+			
+			#0,2,3
+			
+			
+		]
+	)
 	
-	var verts = PackedVector3Array()
-	var uvs = PackedVector2Array()
-	var normals = PackedVector3Array()
-	var indices = PackedInt32Array()
-	pass # Replace with function body.
+	var array = []
+	array.resize(Mesh.ARRAY_MAX)
+	array[Mesh.ARRAY_VERTEX] = vertices
+	array[Mesh.ARRAY_INDEX] = indices
+	array_mesh.add_surface_from_arrays(Mesh.PRIMITIVE_TRIANGLES, array)
+	mesh = array_mesh
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
